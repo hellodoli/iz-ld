2dfcee94f05ff02959cd564e36541bf5b0693674
phatnm3 <phatnm3@fpt.com.vn>
2019-09-05 17:43:33 +0700

    update header.

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ const NAV_DEFAULT_MENU = '#' + NAV_DEFAULT_MENU_NO_HASH;
 // Navigation mobile
 const NAV_MOBILE_NO_HASH = 'izNavMobile';
 const NAV_MOBILE = '#' + NAV_MOBILE_NO_HASH;
+const NAV_MOBILE_MENU_NO_HASH = 'izNavMobileMenu';
+const NAV_MOBILE_MENU = '#' + NAV_MOBILE_MENU_NO_HASH;
 
 function effectSubMenuHover($sfHover, duration, displayStyle = 'block') {
 
@@ -62,34 +64,6 @@ function subMenuHover() {
     if ($sfHover_03.length > 0) effectSubMenuHover($sfHover_03, 200);
 }
 
-function collapseNavMobile() {
-    var $toogleNavMobile = $('#izNavMobileMenu li.menu-item-has-child > a');
-
-    $toogleNavMobile.on('click', function (e) {
-        e.stopPropagation();
-
-        var $prevToggleNavMobile = $(this).next('.sub-menu').find('.opened');
-        if($prevToggleNavMobile.length > 0) {
-            $prevToggleNavMobile.next('.sub-menu').collapse('hide');
-            $prevToggleNavMobile.removeClass('opened');
-            $(this).next('.sub-menu').collapse('hide');
-            return;
-        }
-
-        // add class .opened for toggle current
-        if(!$(this).hasClass('opened')) {
-            $toogleNavMobile.removeClass('opened');//toggle thang ngang hang vs no thoi
-            
-            $(this).addClass('opened');
-        }else{
-            $(this).removeClass('opened');
-        }
-        
-        //toggle .sub-menu
-        $(this).next('.sub-menu').collapse('toggle');
-    });
-}
-
 //--- FOOTER
 function checkFooterFixed($footer) {
     if ($footer.length === 1) {
@@ -118,34 +92,40 @@ function initHeaderAndFooter() {
 
     // 1.2 Navigation Mobile
     //--- add class .collapse
-    var $navMobile = $(NAV_MOBILE);
-    if ($navMobile.length > 0) {
-        $navMobile.addClass('collapse'); // <nav /> parent collapse
-        $navMobile.find('.sub-menu').addClass('collapse'); // all .sub-menu collapse
-
-        var $liParent = $(NAV_MOBILE + ' ' + 'li.menu-item-has-child');
-        if ($liParent.length > 0) {
-            $liParent.each(async (index, element) => {
-                // if has SUB MENU
-                if ($(element).find('li.menu-item-has-child').length > 0) {
-
-                    var $subMenuChild = $(element).children('.sub-menu').find('.sub-menu');
-                    await $(element).attr('id', NAV_MOBILE_NO_HASH + '_' + index);
-
-                    var parentID = $(element).attr('id');
-
-                    $subMenuChild.each((index, element) => {
-                        $(element).attr('data-parent', '#' + parentID);
-                    });
-                }
-            });
-        }
+    var $navSubMenu = $(NAV_MOBILE_MENU + ' .sub-menu');
+    if ($navSubMenu && $navSubMenu.length > 0) {
+        $navSubMenu.css({
+            'display': 'none'
+        });
     }
 
-    //--- collapse navition mobile
-    collapseNavMobile();
+    var $toogleNavMobile = $(NAV_MOBILE_MENU + ' li.menu-item-has-child > a');
+    if ($toogleNavMobile && $toogleNavMobile.length > 0) {
+        $toogleNavMobile.on('click', function () {
+            var $this = $(this);
+            var $subMenu = $this.next('.sub-menu');
+            if ($subMenu && $subMenu.length == 1) {
+                if ($subMenu.hasClass('show')) {
 
-    // 2/FOOTER
+                    $subMenu.removeClass('show');
+                    $subMenu.slideUp(350);
+                    $(this).removeClass('opened');
+
+                } else {
+                    var $parent = $this.parent().parent();
+                    $parent.find('li.menu-item-has-child .sub-menu').removeClass('show');
+                    $parent.find('li.menu-item-has-child .sub-menu').slideUp(350);
+                    $parent.find('li.menu-item-has-child a').removeClass('opened');
+
+                    $this.addClass('opened');
+                    $subMenu.addClass('show');
+                    $subMenu.slideDown(350);
+                }
+            }
+        });
+    }
+
+    // 2/ FOOTER
     var $footer = $('.js-iz-footer');
     checkFooterFixed($footer);
 }
