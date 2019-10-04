f0ae0275cf116ae8ced98dab0122fdb71f59fdff
phatnm3 <phatnm3@fpt.com.vn>
2019-10-04 11:23:13 +0700

    comllete solid scroll.

--- js/index.js
+++ js/index.js
@@ -9,6 +9,12 @@ const NAV_DEFAULT_NO_HASH = 'izNavDefault';
 const NAV_DEFAULT = '#' + NAV_DEFAULT_NO_HASH;
 const NAV_DEFAULT_MENU_NO_HASH = 'izNavDefaultMenu';
 const NAV_DEFAULT_MENU = '#' + NAV_DEFAULT_MENU_NO_HASH;
+
+const NAV_TOGGLE_BURGER_DESKTOP_NO_HASH = 'izMenuBurgerDesktop';
+const NAV_TOGGLE_BURGER_DESKTOP = '#' + NAV_TOGGLE_BURGER_DESKTOP_NO_HASH;
+const NAV_SOLID_MENU_NO_HASH = 'izNavSolidMenu';
+const NAV_SOLID_MENU = '#' + NAV_SOLID_MENU_NO_HASH;
+
 // Navigation mobile
 const NAV_TOGGLE_BURGER_NO_HASH = 'izMenuBurger';
 const NAV_TOGGLE_BURGER = '#' + NAV_TOGGLE_BURGER_NO_HASH;
@@ -16,6 +22,10 @@ const NAV_MOBILE_NO_HASH = 'izNavMobile';
 const NAV_MOBILE = '#' + NAV_MOBILE_NO_HASH;
 const NAV_MOBILE_MENU_NO_HASH = 'izNavMobileMenu';
 const NAV_MOBILE_MENU = '#' + NAV_MOBILE_MENU_NO_HASH;
+// Scroll Header Style
+const SCROLL_NAVBAR_STYLE_DEFAULT = 'SCROLL_NAVBAR_STYLE_DEFAULT';
+const SCROLL_NAVBAR_STYLE_SOLID = 'SCROLL_NAVBAR_STYLE_SOLID';
+
 
 //--- HEADER FUNC
 function effectSubMenuHover($sfHover, duration, displayStyle = 'block') {
@@ -82,7 +92,7 @@ function toggleNavMobile($toogleNavMobile, speed) {
             } else {
                 var $parent = $this.parent().parent();
                 $parent.find('li.menu-item-has-child .sub-menu').removeClass('show');
-                $parent.find('li.menu-item-has-child .sub-menu').slideUp(350);
+                $parent.find('li.menu-item-has-child .sub-menu').slideUp(speed);
                 $parent.find('li.menu-item-has-child a').removeClass('opened');
 
                 $this.addClass('opened');
@@ -93,8 +103,42 @@ function toggleNavMobile($toogleNavMobile, speed) {
     });
 }
 
+function toggleBurgerNavMobile($toggleBurger, $navMobileMenu, $navMobile, navMobileSpeedCollapse) {
+
+
+    $toggleBurger.on('click', function () {
+        var $this = $(this);
+        if ($this.hasClass('opened')) {
+            // close nav mobile
+            if ($navMobileMenu.length > 0) {
+                $navMobileMenu.find('li.menu-item-has-child .sub-menu').removeClass('show');
+                $navMobileMenu.find('li.menu-item-has-child .sub-menu').slideUp(navMobileSpeedCollapse);
+                $navMobileMenu.find('li.menu-item-has-child a').removeClass('opened');
+            }
+
+            if ($navMobile.length > 0) {
+                $navMobile.removeClass('show');
+                $this.removeClass('opened');
+                $navMobile.slideUp(navMobileSpeedCollapse);
+            }
+
+        } else {
+            // open nav mobile
+            if ($navMobile.length > 0) {
+                $this.addClass('opened');
+                $navMobile.addClass('show');
+                $navMobile.slideDown(navMobileSpeedCollapse);
+            }
+        }
+    });
+}
+
 var shouldScrollHide = true;
-function scrollTopWindow($navbarFixed, offsetTop) {
+function scrollNavbarDefault($navbarFixed, offsetTop) {
+
+    console.log('page will scroll DEFAULT style');
+    console.log('offsetTop: ', offsetTop);
+
     const $window = $(window);
     $window.on('scroll', function () {
         if ($window.scrollTop() > offsetTop) {
@@ -122,6 +166,64 @@ function scrollTopWindow($navbarFixed, offsetTop) {
     });
 }
 
+function checkHasClassSolid($navbarFixed) {
+    setTimeout(function() {
+        if( window.pageYOffset === 0 ) {
+            if( !$navbarFixed.hasClass('iz-navbar--transparent') ) $navbarFixed.addClass('iz-navbar--transparent');
+            setTimeout(function() {
+                if( !$navbarFixed.hasClass('iz-navbar--white-text-on-top') ) {
+                    $navbarFixed.addClass('iz-navbar--white-text-on-top');
+                }
+            }, 200);
+        } else {
+            if( $navbarFixed.hasClass('iz-navbar--transparent') ) $navbarFixed.removeClass('iz-navbar--transparent');
+            setTimeout(function() {
+                if( $navbarFixed.hasClass('iz-navbar--white-text-on-top') ) {
+                    $navbarFixed.removeClass('iz-navbar--white-text-on-top');
+                }
+            }, 200);
+        }
+    }, 200);
+}
+
+function scrollNavbarSolid($navbarFixed, offsetTop) {
+
+    console.log('page will scroll SOLID style');
+    console.log('offsetTop: ', offsetTop);
+
+    const $window = $(window);
+    checkHasClassSolid($navbarFixed, $window.scrollTop());
+
+    $window.on('scroll', function () {
+        const $windowScrollTop = $window.scrollTop();
+        if ($windowScrollTop > offsetTop) {
+
+            if(shouldScrollHide) {
+                if ($navbarFixed.hasClass('on-scroll-show')) {
+                    $navbarFixed.removeClass('on-scroll-show');
+                    $navbarFixed.addClass('on-scroll-hide');
+                }
+            }
+            
+            var windowScrollHeight = document.documentElement.scrollHeight;
+            if(windowScrollHeight - window.pageYOffset === window.innerHeight) {
+                if ($navbarFixed.hasClass('on-scroll-hide')) $navbarFixed.removeClass('on-scroll-hide');
+                $navbarFixed.addClass('on-scroll-show');
+                shouldScrollHide = false;
+            }
+        } else {
+
+            checkHasClassSolid($navbarFixed, $windowScrollTop);
+
+            if($navbarFixed.hasClass('on-scroll-hide')) {
+                $navbarFixed.removeClass('on-scroll-hide');
+                $navbarFixed.addClass('on-scroll-show');
+            }
+            shouldScrollHide = true;
+        }
+    });
+}
+
 //--- FOOTER FUNC
 function checkFooterFixed($footer) {
     if ($footer.length === 1) {
@@ -133,10 +235,8 @@ function checkFooterFixed($footer) {
 }
 
 //-- MAIN FUNC
-function initHeaderAndFooter() {
-
-    // 1/ HEADER
-    // 1.1 - Navigation default
+function initHeaderFixedScrollDefault() {
+    console.log('page with DEFAULT NAVBAR');
     //--- make all .sub-menu display: 'none'
     const $navDefaultSubMenu = $(NAV_DEFAULT + ' ' + '.sub-menu');
     if ($navDefaultSubMenu && $navDefaultSubMenu.length > 0) {
@@ -146,9 +246,32 @@ function initHeaderAndFooter() {
             'opacity': 0
         });
     }
-
     //--- hover navigation desk
     navDesktopHover(200);
+}
+
+function initHeaderFixedScrollSolid() {
+    console.log('page with SOLID NAVBAR');
+    const $toggleBurgerDesk = $(NAV_TOGGLE_BURGER_DESKTOP);
+    if($toggleBurgerDesk && $toggleBurgerDesk.length === 1) {
+        const $navSolidMenu = $(NAV_SOLID_MENU);
+        $toggleBurgerDesk.on('click', function() {
+            if( $navSolidMenu && $navSolidMenu.length === 1 ) {
+                $navSolidMenu.addClass('is-open');
+            }
+        });
+    }
+}
+
+function initHeaderAndFooter(scrollNavbarStyle = SCROLL_NAVBAR_STYLE_DEFAULT) {
+
+    // 1/ HEADER
+    // 1.1 - Navigation default
+    if(scrollNavbarStyle === SCROLL_NAVBAR_STYLE_SOLID) {
+        initHeaderFixedScrollSolid();
+    } else {
+        initHeaderFixedScrollDefault();
+    }
 
     // 1.2 - Navigation Mobile
     const navMobileSpeedCollapse = 350;
@@ -157,62 +280,39 @@ function initHeaderAndFooter() {
     const $navMobile = $(NAV_MOBILE);
     const $navMobileSubMenu = $(NAV_MOBILE_MENU + ' .sub-menu');
 
-    if ($navMobile && $navMobile.length === 1) {
-        $navMobile.css({
-            'display': 'none'
-        });
-    }
-
-    if ($navMobileSubMenu && $navMobileSubMenu.length > 0) {
-        $navMobileSubMenu.css({
-            'display': 'none',
-        });
-    }
-
+    if ($navMobile && $navMobile.length === 1) $navMobile.css({ 'display': 'none' });
+    if ($navMobileSubMenu && $navMobileSubMenu.length > 0) $navMobileSubMenu.css({ 'display': 'none' });
+        
     //--- click toggle
-    const $toggleBurger = $(NAV_TOGGLE_BURGER);
-    const $navMobileMenu = $(NAV_MOBILE_MENU);
+    const $toggleBurger = $(NAV_TOGGLE_BURGER); // button burger
+    const $navMobileMenu = $(NAV_MOBILE_MENU); // trigger nav mobile
     const $toogleNavMobile = $(NAV_MOBILE_MENU + ' li.menu-item-has-child > a');
 
     if ($toggleBurger && $toggleBurger.length === 1) {
-        $toggleBurger.on('click', function () {
-            var $this = $(this);
-            if ($this.hasClass('opened')) {
-                // close nav mobile
-                if ($navMobileMenu.length > 0) {
-                    $navMobileMenu.find('li.menu-item-has-child .sub-menu').removeClass('show');
-                    $navMobileMenu.find('li.menu-item-has-child .sub-menu').slideUp(navMobileSpeedCollapse);
-                    $navMobileMenu.find('li.menu-item-has-child a').removeClass('opened');
-                }
-
-                if ($navMobile.length > 0) {
-                    $navMobile.removeClass('show');
-                    $this.removeClass('opened');
-                    $navMobile.slideUp(navMobileSpeedCollapse);
-                }
-
-            } else {
-                // open nav mobile
-                if ($navMobile.length > 0) {
-                    $this.addClass('opened');
-                    $navMobile.addClass('show');
-                    $navMobile.slideDown(navMobileSpeedCollapse);
-                }
-            }
-        });
+        toggleBurgerNavMobile($toggleBurger, $navMobileMenu, $navMobile, navMobileSpeedCollapse);
+    }
+    
+    if ($toogleNavMobile && $toogleNavMobile.length > 0) {
+        toggleNavMobile($toogleNavMobile, navMobileSpeedCollapse);
     }
 
-    if ($toogleNavMobile && $toogleNavMobile.length > 0) toggleNavMobile($toogleNavMobile, navMobileSpeedCollapse);
-
-    // 1.3 - Scroll fixed header
+    // 1.3 - Scroll fixed
     const $navbarFixed = $(NAVBAR_DEFAULT + '.iz-navbar--fixed');
     if ( $navbarFixed && $navbarFixed.length === 1 ) {
-        //--- cal navbar fake
-        const $navbarDefaultFake = $(NAVBAR_DEFAULT_FAKE);
-        if($navbarDefaultFake.length === 1) $navbarDefaultFake.css({ 'height': $(NAVBAR_DEFAULT).outerHeight() });
-        //--- scroll fixed
+
         const offset = $(MAIN).offset().top + 10;
-        scrollTopWindow($navbarFixed, offset);
+
+        if(scrollNavbarStyle === SCROLL_NAVBAR_STYLE_SOLID) {
+            //--- scroll fixed solid
+            scrollNavbarSolid($navbarFixed, offset);
+        } else {
+            //--- cal navbar fake
+            const $navbarDefaultFake = $(NAVBAR_DEFAULT_FAKE);
+            if($navbarDefaultFake.length === 1) $navbarDefaultFake.css({ 'height': $(NAVBAR_DEFAULT).outerHeight() });
+
+            //--- scroll fixed default
+            scrollNavbarDefault($navbarFixed, offset);
+        }
     }
 
     // 2/ FOOTER
